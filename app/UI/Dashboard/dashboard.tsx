6a0ce69ba36fdb59aeae9fd2977468cba6a9975a
Harsh Singh <harshrajeevsingh@gmail.com>
2024-01-09 03:08:32 +0530

    added SideNav

---
+++ app/UI/Dashboard/dashboard.tsx
@@ -0,0 +1,8 @@
+import DashSearch from "./dashSearch";
+export default function Dashboard() {
+  return (
+    <>
+      <DashSearch />
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'lightGray': '#fafafa',
+      'darkGray': '#F2F2F2',
+      'borderGray': '#D9D9D9',
+      'activeGray': '#353C53',
+      'darkText': '#1A181E',
+      'lightText': '#4D4D4D',
+      'searchText': '#808080',
+      'blueText': '#146EB4',
+      'navBlue': '#1E2640',
+      'white': '#FFFFFF',
+      
+    },
   },
   plugins: [],
 }
